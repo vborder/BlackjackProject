d1bfa4255ec08ad2d4708016e53eb5512168d47f
Vincent Borden <vinbor81@gmail.com>
2020-04-10 11:23:09 -0600

    Initial Commit

---
+++ src/com/skilldistillery/blackjack/Card.java
@@ -0,0 +1,51 @@
+package com.skilldistillery.blackjack;
+
+public class Card {
+	private Rank rank;
+	private Suit suit;
+
+	public Card(Rank rank, Suit suit) {
+		this.rank = rank;
+		this.suit = suit;
+	}
+
+	public int getValue() {
+		return rank.getValue();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((rank == null) ? 0 : rank.hashCode());
+		result = prime * result + ((suit == null) ? 0 : suit.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Card other = (Card) obj;
+		if (rank == null) {
+			if (other.rank != null)
+				return false;
+		} else if (!rank.equals(other.rank))
+			return false;
+		if (suit == null) {
+			if (other.suit != null)
+				return false;
+		} else if (!suit.equals(other.suit))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return rank + " of " + suit;
+	}
+}
